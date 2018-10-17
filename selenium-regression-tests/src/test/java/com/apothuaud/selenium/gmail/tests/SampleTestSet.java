0fd3439a43017c30e94dc49d0e4f9add6b07b8c5
Adrian Pothuaud <adrianpothuaud@gmail.com>
2018-10-17 08:47:54 +0200

    [Selenium] Implements : new test for gmail

--- selenium-regression-tests/src/test/java/com/apothuaud/selenium/gmail/tests/SampleTestSet.java
+++ selenium-regression-tests/src/test/java/com/apothuaud/selenium/gmail/tests/SampleTestSet.java
@@ -1,12 +1,21 @@
 package com.apothuaud.selenium.gmail.tests;
 
 import com.apothuaud.selenium.gmail.pages.GMHomePage;
-import com.apothuaud.selenium.google.pages.GHomePage;
+import com.apothuaud.selenium.gmail.pages.GMLoginPage;
+import com.apothuaud.selenium.google.Account;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class SampleTestSet {
 
+    // ToDo: create accounts enum
+    private Account automAccount1 = new Account(
+        "apothuaud-autom-user-1@gmail.com",
+        "AP@@sswo!rd123"
+    );
+
+    // ToDo: Account provider (existing/not)
     @Test(
             groups = { "Gmail", "Sample" },
             //dataProviderClass = com.apothuaud.selenium.google.QueryProvider.class,
@@ -15,11 +24,18 @@ public class SampleTestSet {
             priority = 5,
             successPercentage = 80
     )
-    public void test_login() {
+    public void test_story_1() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/bin/chromedriver");
-        new GMHomePage(new ChromeDriver()).openPage();
-
-
-
+        WebDriver driver = new ChromeDriver();
+        // ToDo: Create pages and methods
+        new GMLoginPage(driver)
+            .openPage()
+            .login(this.automAccount1);
+        new GMHomePage(driver)
+            .clearAllMessages()
+            .verifyNbUnread(0)
+            .sendMail(automAccount1.getMail(), "Test Mail", "Sample mail for automation")
+            .verifyNbUnread(1)
+            .quit();
     }
 }
